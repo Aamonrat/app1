fd48e6cc34623ab7d13dab4cac9266274ccd4c3f
Aamonrat <152828992+Aamonrat@users.noreply.github.com>
2023-12-25 11:03:16 +0700

    Update App.js insert variable
    Update App.js insert variable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import logo from './logo.svg';
 import React from 'react';
 
 function App() {
-    return <h3>Hello World <br/>สวัสดีชาวโลก</h3>
+    let r = 'React'
+    let rn = 'React Native'
+    const el = (
+        <div>
+            Create Web & Mobile Apps<br/>
+            with <span dangerouslySetInnerHTML={{_html: r }}/> & {rn}
+        </div>
+    )
+    return el 
 }
 
 export default App;
