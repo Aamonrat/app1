64f661e5b18860583367ca50bff96e4bfd0b50c8
Aamonrat <152828992+Aamonrat@users.noreply.github.com>
2023-12-25 10:37:07 +0700

    Update App.js Hello World
    Update App.js Hello World

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import React from 'react';
 
 function App() {
-    return <h3 > Hello Wrold! < br / > สวั สดีชาวโลก < /h3>
+    return <h3>Hello World <br/>สวัสดีชาวโลก</h3>
 }
 
 export default App;
