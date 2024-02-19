80acf3f161d3755542ca68e50e5bb13c0def8076
Aamonrat <s6503052412021@email.kmutnb.ac.th>
2024-02-19 11:01:06 +0700

    Add form-text
    Add form-text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import FormSearch from './form-search';
+import FormText from './form-text';
 
 export default function App() {
 
     return(
-       <FormSearch/>
+       <FormText/>
     ) 
 }
